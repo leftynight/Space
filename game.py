64d3004a85dbf9bbf23335aa9b80e19bf2660e1c
drerad <drerad22@gmai.com>
2014-07-23 09:17:26 -0400

    Update game.py

--- game.py
+++ game.py
@@ -22,22 +22,23 @@ from microgame import Microgame
 def make_game():
     # TODO: Return a new instance of your Microgame class.
     #raise NotImplementedError("make_game")
-    return SpaceGame()
+    return LumberjackGame()
 
 def title():
     # TODO: Return the title of the game.
     #raise NotImplementedError("title")
-    return "Space!!!"
+    return "Lumberjack"
 
 def thumbnail():
     # TODO: Return a (relative path) to the thumbnail image file for your game.
     #raise NotImplementedError("thumbnail")
-    return join("games","space","images","thumbnail.png")
+    return join("games", "lumberjack","images", "thumbnail.png")
 
 def hint():
     # TODO: Return the hint string for your game.
     #raise NotImplementedError("hint")
-    return "Fly the spaceship to the planet!"
+    return "Cut down the tree!"
+
 ################################################################################
 
 def _load_image(name, x, y):
@@ -60,72 +61,28 @@ def _load_image(name, x, y):
 ##### MODEL CLASSES ############################################################
 
 # TODO: put your Sprite classes here
-VELOCITY_INC = 15
+class LumberjackSprite(Sprite):
+	def __init__(self):
+		Sprite.__init__(self)
+		lumberjackpath = join("games","lumberjack","images","thumbnail.png")
+		self.image, self.rect = _load_image(lumberjackpath, 300, 200)
 
-class Spaceship(Sprite):
-    def __init__(self):
-        Sprite.__init__(self)
-        shippath = join("games","space","images","spaceship.png")
-        self.image, self.rect = _load_image(shippath, 400 , 600)
-        self.x_velocity = 0
-        self.y_velocity = 0
-
-    def update(self):
-        self.rect.x += self.x_velocity
-        self.rect.y += self.y_velocity
-
-BG_VELOCITY = 3
-class Background(Sprite):
-    def __init__(self):
-        Sprite.__init__(self)
-        bgPath =  join('games','space', 'images', 'space.png')
-        self.image, self.rect = _load_image(bgPath, 0, -1152)
-    
-    def update(self):
-        self.rect = self.rect.move(0, BG_VELOCITY)
-
-
-class Planet(Sprite):
-    def __init__(self):
-        Sprite.__init__(self)
-        planetpath = join("games","space","images","planet.png")
-        self.image, self.rect = _load_image(planetpath, 335, -450)
-
-    def update(self):
-        self.rect = self.rect.move(0, BG_VELOCITY)
-
-class Asteroid(Sprite):
-    def __init__(self, x, y):
-        Sprite.__init__(self)
-        astPath =  join('games','space', 'images', 'asteroid.png')
-        self.image, self.rect = _load_image(astPath, x, y)
-        self.x_velocity = randint(6, 20)
-        self.y_velocity = randint(6, 15)
-    
-    def update(self):
-        self.rect = self.rect.move(self.y_velocity, self.x_velocity)
-        if self.rect.colliderect(spaceship.rect):
-            self.lose()
+class TreeblockSprite(Sprite):
+	def __init__(self):
+		Sprite.__init__(self)
+		treeblockpath = join("games", "lumberjack", "tree-block.png")
+		self.image, self.rect = _load_image(treeblockpath, 300, 100)
 
 ##### MICROGAME CLASS ##########################################################
 
 # TODO: rename this class to your game's name...
-class SpaceGame(Microgame):
+class LumberjackGame(Microgame):
     def __init__(self):
         Microgame.__init__(self)
         # TODO: Initialization code here
-        self.count = 0
-        self.spaceship = Spaceship()
-        self.bg = Background()
-        self.planet = Planet()
-        self.sprites = Group(self.bg, self.planet, self.spaceship)
-
-    def generate_asteroid(self):
-        if self.count == 10:
-            self.sprites.add(Asteroid(-10, randint(0, locals.HEIGHT - 400)))
-            self.count = 0
-        else:
-            self.count += 1
+        self.lumberjack = LumberjackSprite()
+        self.treeblock = TreeblockSprite()
+        self.sprites = Group(self.lumberjack, self.treeblock)
 
     def start(self):
         # TODO: Startup code here
@@ -138,63 +95,10 @@ class SpaceGame(Microgame):
     def update(self, events):
         # TODO: Update code here
         self.sprites.update()
-        
-        #self.generate_asteroid()
-
-        #Check if spaceship hits sides of screen 
-        x_ship_left, _ = self.spaceship.rect.bottomleft
-        x_ship_right, _ = self.spaceship.rect.bottomright
-        if x_ship_left <= 0:
-            self.spaceship.x_velocity = 0
-        elif x_ship_right >= locals.WIDTH - 14:
-            self.spaceship.x_velocity = 0   
-
-        #Check if spaceship hits top/bottom of screen sectioned to movement
-        _, y_ship_top = self.spaceship.rect.topleft
-        _, y_ship_bottom = self.spaceship.rect.bottomleft
-        if y_ship_top <= locals.HEIGHT - 350:
-            self.spaceship.y_velocity = 0
-        elif y_ship_bottom >= locals.HEIGHT - 20:
-            self.spaceship.y_velocity = 0
-
-        #Process user input
-        for event in events:
-            if event.type == KEYDOWN and event.key == K_LEFT:
-                if x_ship_left <= 0:
-                    pass
-                else:
-                    self.spaceship.x_velocity -= VELOCITY_INC
-            elif event.type == KEYUP and event.key == K_LEFT:
-                self.spaceship.x_velocity = 0
-            elif event.type == KEYDOWN and event.key == K_RIGHT:
-                if x_ship_right >= locals.WIDTH - 14:
-                    pass
-                else:
-                    self.spaceship.x_velocity += VELOCITY_INC
-            elif event.type == KEYUP and event.key == K_RIGHT:
-                self.spaceship.x_velocity = 0
-            elif event.type == KEYDOWN and event.key == K_UP:
-                if y_ship_top <= locals.HEIGHT - 350:
-                    pass
-                else:
-                    self.spaceship.y_velocity -= VELOCITY_INC
-            elif event.type == KEYUP and event.key == K_UP:
-                self.spaceship.y_velocity = 0
-            elif event.type == KEYDOWN and event.key == K_DOWN:
-                if y_ship_bottom >= locals.HEIGHT - 20:
-                    pass
-                else:
-                    self.spaceship.y_velocity += VELOCITY_INC
-            elif event.type == KEYUP and event.key == K_DOWN:
-                self.spaceship.y_velocity = 0
-
-        #Make win when spaceship hits planet
-        if self.planet.rect.colliderect(self.spaceship.rect):
-            self.win()
 
     def render(self, surface):
         # TODO: Rendering code here
-        surface.fill(Color(0, 0, 0))
+        surface.fill(Color(255, 255, 255))
         self.sprites.draw(surface)
 
     def get_timelimit(self):
